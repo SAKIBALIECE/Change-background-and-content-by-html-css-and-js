ef415d53203af18d939039bf609848a6605e7fa5
Sakib Ali <sakibalieng@gmail.com>
2024-07-09 01:57:43 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+ function changeContent (getValue){
+    let bg = document.querySelector('.bgcolor');
+    let selectText =document.querySelector('.card');
+    bg.style.background = getValue.value;
+ }
